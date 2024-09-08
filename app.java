8ad31534e9d30e53d24b1cdb19189318029aef16
Khalid Faisal <khalidfaisal0123@gmail.com>
2024-09-08 22:16:46 +0530

    bosdk abhi sekh rha hu

---
+++ app.java
@@ -0,0 +1,13 @@
+
+public class app {
+    public static void main(String[] args) {
+
+   int a = 65;
+int b = 43;
+String teString = "the sum of (a+b) is " + " "+ (a+b);
+System.out.println( teString );
+    
+    }
+
+    
+}
