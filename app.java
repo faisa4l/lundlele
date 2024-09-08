e22d0ac3a6e6d23302eaebb02582dfd5be6026f1
Khalid Faisal <khalidfaisal0123@gmail.com>
2024-09-08 23:20:29 +0530

    add, multi, star

--- app.java
+++ app.java
@@ -1,12 +1,15 @@
 
 public class app {
     public static void main(String[] args) {
-
    int a = 65;
 int b = 43;
 String teString = "the sum of (a+b) is " + " "+ (a+b);
 System.out.println( teString );
-    
+int c = 766;
+int d =455;
+String resutlString = "the product (c*d) is" + " "+ (c*d);
+System.out.println(resutlString);
+System.out.println("hello bacchoo \n*\n**\n***\n***");
     }
 
     
